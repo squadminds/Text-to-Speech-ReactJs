c0476737eafef8408b7f90fa7971be0b378ea01a
Vivek Chaudhary <vivekc@squadmindsinc.com>
2020-01-29 11:28:09 +0530

    session time out for idle active click events been implemented and toaster for notification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import IdleTimer from "react-idle-timer";
+import { ToastContainer, toast } from "react-toastify";
 
 import MessageBox from "./components/MessageBox";
 import List from "./components/List";
@@ -8,20 +9,55 @@ import DisplayContent from "./components/DisplayContent";
 import UserProfile from "./components/UserProfile";
 import Bubble from "./components/Bubble";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Router>
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.idleTimer = null;
+    this.onAction = this._onAction.bind(this);
+    this.onActive = this._onActive.bind(this);
+    this.onIdle = this._onIdle.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Router>
         <Routes />
       </Router> */}
+        <IdleTimer
+          ref={(ref) => {
+            this.idleTimer = ref;
+          }}
+          element={document}
+          onActive={this.onActive}
+          onIdle={this.onIdle}
+          onAction={this.onAction}
+          debounce={250}
+          timeout={9000}
+        />
+        <List />
+        <DisplayContent />
+        {/* <UserProfile /> */}
+        <MessageBox />
+        <ToastContainer />;
+      </div>
+    );
+  }
 
-      <List />
-      <DisplayContent />
-      <UserProfile />
-      {/* <Bubble /> */}
-      <MessageBox />
-    </div>
-  );
+  _onAction(e) {
+    console.log("user did something", e);
+  }
+
+  _onActive(e) {
+    console.log("user is active", e);
+    console.log("time remaining", this.idleTimer.getRemainingTime());
+  }
+
+  _onIdle(e) {
+    console.log("user is idle", e);
+    console.log("last active", this.idleTimer.getLastActiveTime());
+    this.notify();
+  }
+
+  notify = () => toast("Session time out !");
 }
-
-export default App;
